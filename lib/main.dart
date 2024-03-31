2c44d5d7a4976099d17e7d84d18d9c72960ef134
이효연 <ctrl6514@gmail.com>
2024-03-31 21:50:35 +0900

    v0.1

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class GoogleSignInButton extends StatelessWidget {
+  const GoogleSignInButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {}, // Google 로그인 기능 구현
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/google_logo.png',
+              height: 24.0,
+            ),
+            const SizedBox(width: 16.0),
+            const Text('Sign in with Google'),
+          ],
+        ),
+      ),
+    );
+  }
+}
